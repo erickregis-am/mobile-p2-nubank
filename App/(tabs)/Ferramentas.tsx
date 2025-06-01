7ac855e23ee8ae059d070041bbe71aaa361d62ec
Erick Reginaldo <ericktw77@gmail.com>
2025-06-01 02:59:31 -0400

    third commit

---
+++ App/(tabs)/Ferramentas.tsx
@@ -0,0 +1,12 @@
+import { View, Text, Pressable, StyleSheet} from "react-native";
+import { Dimensions } from "react-native";
+
+const { width, height } = Dimensions.get("window");
+
+export default function Ferramentas(){
+    return (
+        <View>
+            
+        </View>
+    );
+}
--- App/_layout.tsx
+++ App/_layout.tsx
@@ -6,8 +6,8 @@ const { width, height } = Dimensions.get("window");
 export default function RootLayout(){
     return (
         <Stack>
-            <Stack.Screen></Stack.Screen>
-            <Stack.Screen></Stack.Screen>
+            <Stack.Screen name="(tabs)" options={{headerShown: false}}></Stack.Screen>
+            <Stack.Screen name="+not-found" options={{headerShown: false}}></Stack.Screen>
         </Stack>
     );
 }
